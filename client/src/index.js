ce08ebb9dc6d54fe439cc9b88c06f61ca9e82880
lixiliu <lixingliu07@gmail.com>
2025-02-28 12:42:00 -0500

    rearrange components and files

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 // import FakeStackOverflow from "./components/fakestackoverflow.js";
-import FakeStackOverflow from "./app/components/fakestackoverflow.tsx";
-import LoginPage from "./app/components/loginpage.js";
-import WelcomePage from "./app/components/welcomepage.js";
-import CreateUser from "./app/components/createuser.js";
+import FakeStackOverflow from "./components/fakestackoverflow.tsx";
+import LoginPage from "./components/loginpage.js";
+import WelcomePage from "./components/welcomepage.js";
+import CreateUser from "./components/createuser.js";
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
